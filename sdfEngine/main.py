626a253890ee7d9b2552192af5264c7497894065
RushiDeshmukhegithub <rushideshmukhe@gmail.com>
2026-01-13 14:11:45 +0530

    Lighting diffusion done for sphere SDF

--- sdfEngine/main.py
+++ sdfEngine/main.py
@@ -40,48 +40,52 @@ float sdSphere(vec3 p, float radius, vec3 center,vec3 camera_pos){
         return length(p-center-camera_pos) - radius;
 }
 
+float map(vec3 p){
+        return sdSphere(p,1.0,vec3(0.0,0.0,5.0),vec3(0.0,0.0,0.0));
+}
 
-
+vec3 getNormal(vec3 p){
+        vec2 e = vec2(0.1,0.0);
+        return normalize(vec3(
+            map(p+e.xyy) - map(p-e.xyy),
+            map(p+e.yxy) - map(p-e.yxy),
+            map(p+e.yyx) - map(p-e.yyx)
+            ));
+}
 
 void main(){
         vec2 uv = gl_FragCoord.xy / u_resolution;
         uv = uv*2.0 - 1.0;
         uv.x *= u_resolution.x/u_resolution.y; 
-        vec3 center = vec3(0.0,0.0,1.0);
-        vec3 ro = vec3(0.0,0.0,3.0);
+        vec3 center = vec3(0.0,0.0,5.0);
+        vec3 ro = camera_pos;
         vec3 normal = normalize(vec3(uv,-1.0));
-        vec3 rd = vec3(uv,-1.0);
+        vec3 rd = vec3(uv,1.0);
+        //ro *= angle;
+        //rd *= angle;
         float t = 0.0;
         vec3 p = ro;
-        for(int i=0;i<64;i++){
+        for(int i=0;i<256;i++){
             vec3 p = ro + rd*t;
-            float d = sdSphere(p,0.5,center,camera_pos);
-            if(d<0.001){
-                break;
-            }
+            float d = sdSphere(p,1.0,center,vec3(0.0,0.0,0.0));
             t+=d;
-            if(d>10.0){
+            if(abs(d)<0.001){
+                break;
+            }
+            if(t>500.0){
                 break;
             }
         }
-        vec3 light_ray = p - light_source;
-        vec3 view_ray = p - camera_pos;
-        vec3 normal_ray = p - center;
-        float diff = max(dot(light_ray,normal_ray),0.0)/(length(light_ray)*length(normal_ray))  + 
-                     max(dot(view_ray,normal_ray),0.0)/(length(view_ray)*length(normal_ray));
-        if(t < 10.0){
-            //f_color = vec4(0.5,1.0*((10.1-t)/10.0),0.0,1.0);
-            f_color = vec4(0.0,1.0*(1.0-min(abs(diff),0.9)),0.0,1.0);
+        vec3 light_ray = light_source - p;
+        vec3 col
+        //;
+        =vec3(0.1,0.1,0.1);
+        if(t < 500.0){
+            p = ro+rd*t;
+            vec3 diffusion = vec3(1)*clamp(dot(getNormal(p),normalize(light_source-p)),0.0,1.0);
+            col += diffusion;
         }
-        else{
-            f_color = vec4(0.0,0.0,0.0,1.0);
-        }
-        if(abs(uv.x) < 0.005){
-            f_color = vec4(1.0,0.0,0.0,1.0); 
-        }
-        if(abs(uv.y) < 0.005){
-            f_color = vec4(0.0,0.0,1.0,1.0); 
-        }
+        f_color = vec4(col,1.0);
 }
 
 """
@@ -89,7 +93,7 @@ void main(){
 program = ctx.program(vertex_shader,fragment_shader)
 camera_pos = [0.0,0.0,0.0]
 program['camera_pos'].value = camera_pos
-light_source = [0.0,0.0,0.0];
+light_source = [20.0,40.0,-30.0];
 program['light_source'].value = light_source
 u_resolution = program['u_resolution']
 u_resolution.value = (WIDTH,HEIGHT)
@@ -118,9 +122,9 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
             if event.key == pygame.K_w:
-                camera_pos[2] += 0.1
+                camera_pos[2] += 1.0
             if event.key == pygame.K_s:
-                camera_pos[2] -= 0.1
+                camera_pos[2] -= 1.0
             if event.key == pygame.K_a:
                 camera_pos[0] -= 0.1
             if event.key == pygame.K_d:
@@ -145,7 +149,7 @@ while running:
     ctx.clear(0.0,0.0,0.0)
     program['camera_pos'].value = camera_pos
     program['light_source'].value = light_source
-    vao.render(mode=moderngl.TRIANGLES)
+    vao.render()
     pygame.display.flip()
 
 vbo.release()
